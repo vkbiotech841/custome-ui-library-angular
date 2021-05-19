2aabbd472c7d272b7f400c77482b66ba06570d0c
VIKRAM KUMAR <vkbiotech841@gmail.com>
2021-05-19 18:42:12 +0530

    font family added

--- projects/uitest/src/app/app.module.ts
+++ projects/uitest/src/app/app.module.ts
@@ -5,7 +5,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { FontsModule } from 'ui';
+// ONE WAY:
+// This is a correct way to import as file. if you save any changes need to re-build the application everytime. otherwise, changes will not be reflected.
+// import { FontsModule } from 'ui';
+
+
+// ANOTHER WAY: HACK
+// Import the module that you wish to currently working to chages component. So, need not to rebuild all the time.
+
+import { FontsModule } from 'projects/ui/src/lib/fonts/fonts.module';
 
 
 
